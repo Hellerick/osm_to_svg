5f703dde2175c9d6cf572d90b20eed4e638b587a
Hellerick <hellerickferlibay@gmail.com>
2017-02-03 00:39:01 +0700

    From home

--- osm_to_svg.py
+++ osm_to_svg.py
@@ -1,6 +1,17 @@
 import xml.etree.ElementTree as ET
 # https://docs.python.org/3/library/xml.etree.elementtree.html
-osm_path = r'C:\Users\User\Dropbox\Job-shared\Conworlding\Opengeofiction\Latania\Latania.osm'
-print(osm_path)
-tree = ET.parse(osm_path)
-print(tree)
+
+example_osm_path = r'/home/hellerick/PycharmProjects/osm_to_svg/Vesunsoy.osm'
+
+def generate_svg_from_osm(osm_path):
+    print(f'OSM file path: {osm_path}')
+    tree = ET.parse(osm_path)
+    root = tree.getroot()
+    for child in root:
+        print(child.tag, child.attrib)
+        for subchild in child:
+            print(' >', subchild.tag, subchild.attrib)
+    # print(tree)
+
+if __name__ == '__main__':
+    generate_svg_from_osm(example_osm_path)
