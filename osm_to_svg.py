36a71872b463045af83cbcff54fe0a0f5a5c909a
Hellerick <hellerickferlibay@gmail.com>
2017-02-02 17:58:00 +0700

    1704

--- osm_to_svg.py
+++ osm_to_svg.py
@@ -1,3 +1,6 @@
 import xml.etree.ElementTree as ET
+# https://docs.python.org/3/library/xml.etree.elementtree.html
 osm_path = r'C:\Users\User\Dropbox\Job-shared\Conworlding\Opengeofiction\Latania\Latania.osm'
 print(osm_path)
+tree = ET.parse(osm_path)
+print(tree)
