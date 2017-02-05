94561d5106880fe084e8f2d9ac4b79079275d36c
Hellerick <hellerickferlibay@gmail.com>
2017-02-06 00:42:46 +0700

    From home

--- osm_to_svg.py
+++ osm_to_svg.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import platform
+import re
 from math import pi, log, tan
 
 max_image_size = 1200
@@ -12,11 +13,17 @@ example_osm_path = {
     'DESKTOP-62BVD4A': 'd:\KPV\Github\osm_to_svg\Vesunsoy.osm',
     'hellerick-C17A': r'/home/hellerick/PycharmProjects/osm_to_svg/Vesunsoy.osm'}[platform.node()]
 
+# example_osm_path = '/home/hellerick/Dropbox/Job-shared/Conworlding/Opengeofiction/Latania/Latania.osm'
+
 
 def mercatorize(lat):
     lat *= deg
     y = log(tan(pi / 4 + lat / 2))
     return y / deg
+
+
+def round3(n):
+    return round(n*1000)/1000
 
 
 class Bounds:
@@ -51,6 +58,20 @@ class Node:
         self.plot = (self.x, self.y)
 
 
+class Way:
+    def __init__(self, way, nodes):
+        self.id = int(way.attrib['id'])
+        self.nd = [nodes[int(n.attrib['ref'])] for n in way.findall('nd')]
+        self.d = "M " + ' L '.join(f'{round3(n.x)} {round3(n.y)}' for n in self.nd)
+
+        try:
+            # print('Way tag attribs:', way.findall('tag')[0].attrib)
+            way_type = [t for t in way.findall('tag') if not t.attrib['k'] in ('source', 'name')][0]
+            self.type = way_type.attrib['k']+' - '+way_type.attrib['v']
+        except IndexError:
+            self.type = 'none'
+
+
 def generate_svg_from_osm(osm_path):
     print(f'OSM file path: {osm_path}')
     tree = ET.parse(osm_path)
@@ -72,14 +93,49 @@ def generate_svg_from_osm(osm_path):
     #         pass
     # print('...')
 
+    '''Creating SVG'''
+    svg_canvas = ET.Element('svg')
+    ET.register_namespace('inkscape', "http://www.inkscape.org/namespaces/inkscape")
+    svg_canvas.set('xmlns', 'http://www.w3.org/2000/svg')
+    svg_canvas.set('xmlns:inkscape', 'http://www.inkscape.org/namespaces/inkscape')
+    svg_canvas.set('version', '1.1')
+    svg_canvas.set('width', str(round3(bounds.canvas[0])))
+    svg_canvas.set('height', str(round3(bounds.canvas[1])))
+
     nodes = {int(n.attrib['id']): Node(n, bounds) for n in root.findall('node')}
-    for i in range(1, 10):
-        print(f'{i}, lat: {nodes[i].lat:0.6f}, lon: {nodes[i].lon:0.6f}, plot: {nodes[i].x:0.6f} {nodes[i].y:0.6f}')
+    # for i in range(1, 10):
+    #     print(f'{i}, lat: {nodes[i].lat:0.6f}, lon: {nodes[i].lon:0.6f}, plot: {nodes[i].x:0.6f} {nodes[i].y:0.6f}')
     # nodes = {int(n.attrib['id']):(float(n.attrib['lat']), float(n.attrib['lon'])) for n in nodes}
 
     # mercatorize all nodes
     # nodes = {n:mercatorize(nodes[n]) for n in nodes}
     print('Nodes:', nodes)
+
+    ways = {int(w.attrib['id']): Way(w, nodes) for w in root.findall('way')}
+
+    way_layers = sorted(list({ways[w].type for w in ways}))
+    print('Way layers:', way_layers)
+    for l in way_layers:
+        group = ET.Element('g')
+        group.set('id', re.sub(' ', '_', l))
+        group.set('inkscape:label', l)
+        group.set('inkscape:groupmode', 'layer')
+        group.extend(
+                (
+                    ET.Element('path', fill='none', stroke='black', d=ways[w].d)
+                    for w in ways if ways[w].type == l
+                )
+            )
+        svg_canvas.extend([group])
+
+    # svg_canvas.extend([ET.Element('path', fill='none', stroke='black', d=ways[w].d) for w in ways])
+
+    svg_path = osm_path+'.svg'
+    print(f'\nWriting SVG at\n{svg_path}')
+    ET.ElementTree(svg_canvas).write(svg_path, xml_declaration=True, encoding='utf-8', method='xml')
+    # with open(svg_path, mode='wb') as svg_file:
+    #
+    #     svg_file.write(ET.tostring(svg_canvas))
 
 
 if __name__ == '__main__':
