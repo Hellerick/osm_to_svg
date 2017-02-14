083298e1ec0e97b6fa91b8ac38844be242476d1f
Hellerick <hellerickferlibay@gmail.com>
2017-02-14 14:01:57 +0700

    From Severboat

--- osm_to_svg.py
+++ osm_to_svg.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
+import os
 import platform
 import re
+import requests
 from math import pi, log, tan
 
 max_image_size = 1200
@@ -9,9 +11,12 @@ deg = pi / 180
 # https://docs.python.org/3/library/xml.etree.elementtree.html
 
 
-example_osm_path = {
-    'DESKTOP-62BVD4A': 'd:\KPV\Github\osm_to_svg\Vesunsoy.osm',
-    'hellerick-C17A': r'/home/hellerick/PycharmProjects/osm_to_svg/Vesunsoy.osm'}[platform.node()]
+project_path = {
+    'DESKTOP-62BVD4A': 'd:\KPV\Github\osm_to_svg',
+    'hellerick-C17A': r'/home/hellerick/PycharmProjects/osm_to_svg'}[platform.node()]
+
+
+example_osm_path = os.path.join(project_path, 'esunsoy.osm')
 
 example_osm_path = 'd:\KPV\Maps\Moscow railroads\overpass-api.de - api - xapi - way bbox=36.7163086,55.0594952,38.0566406,56.1026834 railway=rail.osm'
 
@@ -27,33 +32,37 @@ def round3(n):
 
 
 class Bounds:
-    def __init__(self, root):
-        try:
-            bounds = root.find('bounds').attrib
-        except AttributeError:
-            nodes = root.findall('node')
-            bounds = dict(
-                    minlat=min([float(n.attrib['lat']) for n in nodes]),
-                    maxlat=max([float(n.attrib['lat']) for n in nodes]),
-                    minlon=min([float(n.attrib['lon']) for n in nodes]),
-                    maxlon=max([float(n.attrib['lon']) for n in nodes]),
-                )
-        self.bounds = bounds
-        self.latmin = float(bounds['minlat'])
-        self.latmax = float(bounds['maxlat'])
-        if self.latmax < self.latmin:
-            self.latmax += 360.0
-        self.lonmin = float(bounds['minlon'])
-        self.lonmax = float(bounds['maxlon'])
-        self.mermax = mercatorize(self.latmax)
-        self.mermin = mercatorize(self.latmin)
-        self.lonran = self.lonmax - self.lonmin
-        self.latran = self.latmax - self.latmin
-        self.merran = self.mermax - self.mermin
-        if self.merran > self.lonran:
-            self.canvas = (1200.0 * self.lonran / self.merran, 1200.0)
+    def __init__(self, arg): # root=None, bounds=None
+        if type(arg) == dict:
+            bounds = arg
         else:
-            self.canvas = (1200.0, 1200.0 * self.merran / self.lonran)
+            root = arg
+            try:
+                bounds = root.find('bounds').attrib
+            except AttributeError:
+                nodes = root.findall('node')
+                bounds = dict(
+                        lat_min=min([float(n.attrib['lat']) for n in nodes]),
+                        lat_max=max([float(n.attrib['lat']) for n in nodes]),
+                        lon_min=min([float(n.attrib['lon']) for n in nodes]),
+                        lon_max=max([float(n.attrib['lon']) for n in nodes]),
+                    )
+        self.bounds = bounds
+        self.lat_min = float(bounds['lat_min'])
+        self.lat_max = float(bounds['lat_max'])
+        if self.lat_max < self.lat_min:
+            self.lat_max += 360.0
+        self.lon_min = float(bounds['lon_min'])
+        self.lon_max = float(bounds['lon_max'])
+        self.mer_max = mercatorize(self.lat_max)
+        self.mer_min = mercatorize(self.lat_min)
+        self.lon_ran = self.lon_max - self.lon_min
+        self.lat_ran = self.lat_max - self.lat_min
+        self.mer_ran = self.mer_max - self.mer_min
+        if self.mer_ran > self.lon_ran:
+            self.canvas = (1200.0 * self.lon_ran / self.mer_ran, 1200.0)
+        else:
+            self.canvas = (1200.0, 1200.0 * self.mer_ran / self.lon_ran)
 
 
 class Node:
@@ -62,8 +71,8 @@ class Node:
         self.lat = float(node.attrib['lat'])
         self.lon = float(node.attrib['lon'])
         self.mer = mercatorize(self.lat)
-        self.x = (self.lon - bounds.lonmin) / bounds.lonran * bounds.canvas[0]
-        self.y = bounds.canvas[1] - (self.mer - bounds.mermin) / bounds.merran * bounds.canvas[1]
+        self.x = (self.lon - bounds.lon_min) / bounds.lon_ran * bounds.canvas[0]
+        self.y = bounds.canvas[1] - (self.mer - bounds.mer_min) / bounds.mer_ran * bounds.canvas[1]
         self.plot = (self.x, self.y)
 
 
@@ -87,8 +96,8 @@ def generate_svg_from_osm(osm_path):
     root = tree.getroot()
     bounds = Bounds(root)
     print(
-        f'Bounds, lat: {bounds.latmin:0.6f}..{bounds.latmax:0.6f} ({bounds.latran:0.6f}); '
-        f'lon: {bounds.lonmin:0.6f}..{bounds.lonmax:0.6f} ({bounds.lonran:0.6f})')
+        f'Bounds, lat: {bounds.lat_min:0.6f}..{bounds.lat_max:0.6f} ({bounds.lat_ran:0.6f}); '
+        f'lon: {bounds.lon_min:0.6f}..{bounds.lon_max:0.6f} ({bounds.lon_ran:0.6f})')
 
     print('Canvas (width, height):', bounds.canvas)
 
@@ -149,6 +158,29 @@ def generate_svg_from_osm(osm_path):
 # Downloading a certain type
 # http://www.overpass-api.de/api/xapi?way[bbox=36.7163086,55.0594952,38.0566406,56.1026834][railway=rail]
 
+def download_osm(bounds, selecttion):
+    print('== Download OSM data ==')
+    base_url = 'http://www.overpass-api.de/api/xapi?'
+    query = f'way[bbox={bounds.lon_min},{bounds.lat_min},{bounds.lon_max},{bounds.lat_max}]'
+    for key in selection:
+        query = query + f'[{key}={selection[key]}]'
+    print('Query:', base_url+query)
+    osm_file_name = re.sub(r'(\[|\])+', ' ', query).strip()+'.osm'
+    osm_file_path = os.path.join(project_path, osm_file_name)
+    # osm_file_name = ''.join([c for c in query if c.isalnum() or c in ',.' else ' '])
+    data = requests.get(base_url+query).text
+    with open(osm_file_path, mode='wt') as f:
+        f.write(data)
+    print(f'Saved as: {osm_file_path}')
+    return osm_file_path
+
+
+
 if __name__ == '__main__':
-    download_osm(box=)
-    generate_svg_from_osm(example_osm_path)
+    bounds = Bounds(dict(lon_min=35.1435, lon_max=40.2035, lat_min=54.2557, lat_max=56.9611))
+    selection = {
+        'railway': 'rail',
+        'admin_level': '4',
+    }
+    osm_file_path = download_osm(bounds, selection)
+    # generate_svg_from_osm(example_osm_path)
